c56e76c3e7d652d23e0f60719706ebfdbf5f67e6
nbari <nbari@tequila.io>
2016-11-17 15:37:21 +0100

    version 0.3.0 	modified:   www.rb

--- www.rb
+++ www.rb
@@ -1,9 +1,9 @@
 class Www < Formula
   desc "static web server"
   homepage "https://github.com/nbari/www"
-  url "https://bintray.com/nbari/www/download_file?file_path=www_0.2.0_darwin_amd64.zip"
-  sha256 "9a45e72eeeff00f6bf36b20dd0f257a91bea71302514e9004a1df90d28d84168"
-  version "0.2.0"
+  url "https://bintray.com/nbari/www/download_file?file_path=www_0.3.0_darwin_amd64.zip"
+  sha256 "45d6b8886705f7cfe3db7ed64fc855de213d5d14e36217231614ceb03e34b402"
+  version "0.3.0"
 
   def install
     bin.install "www"
