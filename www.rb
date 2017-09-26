eddeb3d923d6320070a2d8dc0547ba5986391632
nbari <nbari@tequila.io>
2017-09-26 21:56:40 +0200

    1.1.1

--- www.rb
+++ www.rb
@@ -1,35 +1,27 @@
-require "language/go"
-
 class Www < Formula
-  desc "static web server"
+  desc "HTTPS? static web server"
   homepage "https://github.com/nbari/www"
   url "https://github.com/nbari/www.git",
-      :tag => "1.0.0",
-      :revision => "5bd382cdd2fefd4f8dbf70f3f0085ba7a411510e"
+      :tag => "1.1.1",
+      :revision => "88e655e483e714d45fdcca1a5054c69d04cab75c"
 
   head "https://github.com/nbari/www.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "179e07bb22e2755e3d082f07607cf2e98e266defaace3ceb764a4c32f03ccf8d" => :sierra
-    sha256 "5be8ef01a8568b7de75b19d525a4830b0c934c43b9e8f9fb150cbdcb18091a26" => :el_capitan
-    sha256 "6dcf9d7de2489f5e3d6fd56488c3abe2bff1ea6f6bf0d9de89421b0e9a7e2e38" => :yosemite
-  end
-
   depends_on "go" => :build
-
+  depends_on "dep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/nbari/www").install buildpath.children
-    Language::Go.stage_deps resources, buildpath/"src"
     cd "src/github.com/nbari/www" do
-      ldflags = "-s -w -X main.version=#{version}"
+      system "dep", "ensure"
+      ldflags = "-s -w"
       system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/www"
+      prefix.install_metafiles
     end
   end
 
   test do
-    system bin/"www", "-v"
+    system bin/"www", "-h"
   end
 end
